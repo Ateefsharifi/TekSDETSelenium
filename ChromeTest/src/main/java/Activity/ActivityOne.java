9726d1d5ab41640cdc1229ba8b7cf4d49a74d6c8
Admin <ateefsharifi@tekschool.com>
2024-07-16 19:59:45 -0400

    First Example

--- ChromeTest/src/main/java/Activity/ActivityOne.java
+++ ChromeTest/src/main/java/Activity/ActivityOne.java
@@ -7,11 +7,11 @@ import org.openqa.selenium.chrome.ChromeDriver;
 
 public class ActivityOne {
     public static void main(String[] args){
-        WebDriver driver=new ChromeDriver();
+   //     WebDriver driver=new ChromeDriver();
         //locate the element
-        By SearchInputLocator=By.id("SearchInput");
+   //     By SearchInputLocator=By.id("SearchInput");
         //Find the Element
-        WebElement SearchInputElement=driver.findElement(SearchInputLocator);
+  //      WebElement SearchInputElement=driver.findElement(SearchInputLocator);
         //perform the action
         
 
