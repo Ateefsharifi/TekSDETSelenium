0989c5d3981255b0ee31ca35f0b26ea0ed26ce24
Admin <ateefsharifi@tekschool.com>
2024-07-18 20:01:58 -0400

    Navigation Test

---
+++ TekClass/src/main/java/Activity/SyncronizationActivity.java
@@ -0,0 +1,36 @@
+package Activity;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.time.Duration;
+import java.util.concurrent.TimeUnit;
+
+public class SyncronizationActivity {
+    public static void main(String[] args){
+        WebDriver driver=new ChromeDriver();
+        driver.get("https://retail.tekschool-students.com/");
+        driver.manage().window().maximize();
+
+        driver.manage().timeouts().implicitlyWait(Duration.ofDays(TimeUnit.SECONDS.toSeconds(10)));
+
+        driver.findElement(By.id("signinLink")).click();
+        driver.findElement(By.id("email")).sendKeys("Example@gmail.com");
+        driver.findElement(By.id("password")).sendKeys("Test@12345");
+        driver.findElement(By.id("loginBtn")).click();
+
+        WebDriverWait Wait=new WebDriverWait(driver,Duration.ofSeconds(10));
+
+        driver.findElement(By.id("accountLink")).click();
+        driver.findElement(By.id("personalPhoneInput")).clear();
+        driver.findElement(By.id("personalPhoneInput")).sendKeys("7030000000");
+        driver.findElement(By.id("personalUpdateBtn")).click();
+
+
+
+
+
+    }
+}
